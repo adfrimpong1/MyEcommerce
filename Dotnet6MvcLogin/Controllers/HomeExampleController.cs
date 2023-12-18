9a57d2a73e506138d4f977591662f1c5e33485f6
Adfrimpong1 <derrickfimpongfrimpong@gmail.com>
2023-12-18 16:27:12 +0000

    HomeExampleController

---
+++ Dotnet6MvcLogin/Controllers/HomeExampleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet6MvcLogin.Controllers
+{
+    public class HomeExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
